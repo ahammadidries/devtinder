6a0da8672c4580b00950b423c62c2999d77dbcda
ahammadidries <ahammadidries.ai@gmail.com>
2025-07-27 04:54:39 +0530

    user API and Feed API

--- src/app.js
+++ src/app.js
@@ -13,6 +13,34 @@ app.post("/signup", async (req, res)=>{
     res.send("User data sended successfully");
 });
 
+//Get user by email
+app.get("/user", async (req,res)=> {
+   
+   const userEmail = req.body.emailId;
+
+   try {
+
+        const users = await User.find({emailId: userEmail});
+
+        res.send(users);
+   } catch(err) {
+
+    res.status(400).send("Something went worng");
+
+   }
+});
+
+// Feed API - GET /feed - get all the users froom the database
+app.get("/feed", async (req , res)=> {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (err) {
+        res.status(404).send("Users not found");
+        
+    }
+});
+
 
 connectDB()
     .then(()=> {
